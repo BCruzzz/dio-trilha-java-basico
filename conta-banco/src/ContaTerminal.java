3b81c22b4edd82a1b32f7ed55e18b8e41e47c8cd
BCruzzz <bmcruzz@hotmail.com>
2025-06-27 03:06:36 -0300

    programa finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        //Classe scanner para receber os valores pelo usuario via terminal
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome completo: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        String agency = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int accountNumber = scanner.nextInt();
+
+        System.out.println("Digite o seu saldo atual: ");
+        float balance = scanner.nextFloat();
+
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + accountNumber + " e seu saldo " + balance + " já está disponível para saque");
+
 
        }
     }
